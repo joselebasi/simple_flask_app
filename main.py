8856dac22f9bbcdd0aaf9deac10ac89b89dc33dd
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-01-25 07:43:36 -0600

    INit 7

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ from currency_converter import CurrencyConverter
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def form():
     return render_template("form.html")
-
 
 @app.route("/", methods=["POST"])
 def my_form_post():
     c = CurrencyConverter()
 
     euros = request.form["euros"]
+
     usd = round(c.convert(euros, "EUR", "USD"), 2)
 
     return render_template("form.html", euros=euros, usd=usd)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
