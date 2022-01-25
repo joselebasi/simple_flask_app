d66bfb72dc49fb6960bd243678cfeabf4799cdec
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-01-25 07:58:40 -0600

    INit 9

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from flask import Flask
 from flask import render_template, request
@@ -6,14 +7,19 @@ from currency_converter import CurrencyConverter
 
 app = Flask(__name__)
 
+stdout_fileno = sys.stdout 
+sample_input = ['Hi', 'Hello from AskPython', 'exit']
+
 @app.route("/")
 def form():
     return render_template("form.html")
 
 @app.route("/", methods=["POST"])
 def my_form_post():
-
-    app.logger.warning(request.data)
+    
+    for ip in sample_input:
+        # Prints to stdout
+        stdout_fileno.write(ip + '\n')
 
     c = CurrencyConverter()
 
@@ -25,4 +31,4 @@ def my_form_post():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
