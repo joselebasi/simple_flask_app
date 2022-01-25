7d425bbf0676eadaa81b3d10cd1658658433836a
Jose I Toledano Morales <jose.toledano@acigroup.com.mx>
2022-01-25 00:15:21 -0600

    INit 3

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask,Response,jsonify, render_template ,logging,request
+import os
+
+from flask import Flask
+from flask import render_template, request
+from currency_converter import CurrencyConverter
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-#run server
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/", methods=["POST"])
+def my_form_post():
+    c = CurrencyConverter()
+
+    euros = request.form["euros"]
+    usd = round(c.convert(euros, "EUR", "USD"), 2)
+
+    return render_template("form.html", euros=euros, usd=usd)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
